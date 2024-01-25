be6b52bbe9d5075875413ed7146569b32e28dd4c
n-someya <n.someya.ynu@gmail.com>
2024-01-25 23:53:37 +0900

    05_chapter5-erc-721-ui-v2: 第3章 5.4まで完了

--- blockchainApp/frontend/app/mynft/page.tsx
+++ blockchainApp/frontend/app/mynft/page.tsx
@@ -1,13 +1,20 @@
 'use client'
-import { ethers } from 'ethers';
+import { ethers, isError } from 'ethers';
 import { useContext, useEffect, useRef, useState } from 'react';
-import artifact from '../../abi/MyERC721.sol/MyERC721.json';
 import { Web3SignerContext } from '@/context/web3.context';
 import {
   Alert, Avatar, Button, Card, Container, Group, SimpleGrid, Stack,
-  Text, TextInput, Title
+  Text, TextInput, Title, Image, Badge
 } from '@mantine/core';
 import { IconCubePlus } from '@tabler/icons-react';
+import { MyERC721, MyERC721__factory } from "@/types";
+
+type NFT = {
+  tokenId: bigint,
+  name: string,
+  description: string,
+  image: string,
+};
 
 // デプロイしたMyERC721 Contractのアドレスを入力
 const contractAddress = '0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0';
@@ -19,16 +26,12 @@ export default function MyNFT() {
   const { signer } = useContext(Web3SignerContext);
 
   // MyERC721のコントラクトのインスタンスを保持するState
-  const [myERC721Contract, setMyERC721Contract] = useState<ethers.Contract | null>(null);
+  const [myERC721Contract, setMyERC721Contract] = useState<MyERC721 | null>(null);
 
   // MyERC721のコントラクトのインスタンスをethers.jsを利用して生成
   useEffect(() => {
     // MyERC721コントラクトの取得
-    const contract = new ethers.Contract(
-      contractAddress,
-      artifact.abi,
-      signer
-    );
+    const contract = MyERC721__factory.connect(contractAddress, signer);
     setMyERC721Contract(contract);
     // NFT作成フォームのデフォルト値として、現在のアカウントアドレスを設定
     const fillAddress = async () => {
@@ -61,6 +64,47 @@ export default function MyNFT() {
       setLoading(false);
     }
   };
+
+  // 保有するNFTの一覧を生成
+  const [myNFTs, setMyNFTs] = useState<NFT[]>([]);
+  // MyERC721コントラクトを呼び出して、自身が保有するNFTの情報を取得
+  useEffect(() => {
+    const fetchMyNFTs = async () => {
+      const nfts = [];
+      if (myERC721Contract && myERC721Contract.runner) {
+        const myAddress = signer?.getAddress()!
+        // 自分が保有するNFTの総数を確認
+        let balance = BigInt(0);
+        try {
+          balance = await myERC721Contract.balanceOf(myAddress)
+        } catch (err) {
+          if (isError(err, "BAD_DATA")) {
+            // balanceOfにおいて、対応アドレスの保有NFTが0のときは、BAD_DATAエラーが発生するためハンドリング
+            balance = BigInt(0);
+          } else {
+            throw err;
+          }
+        }
+        for (let i = 0; i < balance; i++) {
+          // ERC721Enumerableのメソッドを利用して、インデックスから自身が保有するNFTのtokenIdを取得
+          const tokenId = await myERC721Contract.tokenOfOwnerByIndex(myAddress, i);
+          // NOTE：本来は下記のようにtokenURIからJson Metadataを取得しNFTのコンテンツ情報にアクセス
+          // const tokenURI = await myERC721Contract.tokenURI(tokenId);
+          // const response = await fetch(tokenURI);
+          // const jsonMetaData = await response.json();
+          // NOTICE: 下記は画面表示のためのダミーデータ
+          const jsonMetaData = {
+            name: `NFT #${tokenId}`,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscingelit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            image: `https://source.unsplash.com/300x200?glass&s=${tokenId}`
+          }
+          nfts.push({ tokenId, ...jsonMetaData });
+        }
+        setMyNFTs(nfts);
+      }
+    };
+    fetchMyNFTs();
+  }, [myERC721Contract, signer]);
 
   // NFT作成のSuccess Alert
   const [showAlert, setShowAlert] = useState(false); // Alertの表示管理
@@ -105,7 +149,30 @@ export default function MyNFT() {
             <Button loading={loading} onClick={handleButtonClick}>Mint NFT</Button>
           </Stack>
         </Card>
-      </SimpleGrid>
-    </div>
+        {/* NFT一覧 */}
+        {
+          myNFTs.map((nft, index) => (
+            <Card key={index} shadow="sm" padding="lg" radius="md" withBorder>
+              <Card.Section>
+                <Image
+                  src={nft.image}
+                  height={160}
+                  alt="No image"
+                />
+              </Card.Section>
+              <Group justify="space-between" mt="md" mb="xs">
+                <Text fw={500}>{nft.name}</Text>
+                <Badge color="blue" variant="light">
+                  tokenId: {nft.tokenId.toString()}
+                </Badge>
+              </Group>
+              <Text size="sm" c="dimmed">
+                {nft.description}
+              </Text>
+            </Card>
+          ))
+        }
+      </SimpleGrid >
+    </div >
   );
 }
--- blockchainApp/hardhat.config.ts
+++ blockchainApp/hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  typechain: {
+    outDir: 'frontend/types',
+    target: 'ethers-v6',
+    alwaysGenerateOverloads: false, // コントラクトにおける関数のオーバーロードがない場合でも、"deposit(uint256)"のような完全なシグネチャを生成するか
+    // externalArtifacts: ['externalArtifacts/*.json'], // Typeファイルの生成に追加したい外部のArtifactsがある場合は指定する
+    },
 };
 
 export default config;
