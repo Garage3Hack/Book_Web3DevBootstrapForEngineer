3f0fb929d36a3b0ce034b0ec62b8a02c2867d12d
aikeim <maikei@tohmatsu.co.jp>
2024-03-01 03:13:14 +0900

    11_appendix-poanet: POAネットワークの構築

--- blockchainApp/hardhat.config.ts
+++ blockchainApp/hardhat.config.ts
@@ -1,8 +1,15 @@
+import 'dotenv/config'
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    poanet: {
+      url: `${process.env.POANET_URL}`,
+      accounts: [`0x${process.env.POANET_PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
